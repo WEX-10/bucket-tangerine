82606667fae59e6938df58d7954d0c53bf9c2c97
WEX 10 <work-experience10@outlook.com>
2026-07-15 13:56:43 +0100

    Forgot to save the p1.2 file before commit :(

--- rest/create_fact.py
+++ rest/create_fact.py
@@ -17,10 +17,11 @@ def create_route():
 
         # TODO: (Task P1.2) Check that fact text is provided, if not return an error
         if factt == None:
-            raise ValueError("No Fact Provided")
-
+            raise ValueError("No Fact Provided by Users")
         # TODO: (Task P1.2) Call the create_fact function from the database folder
-        #fact = create_fact(factt)
+        fact = create_fact(factt)
+        if fact.id == None:
+            raise ValueError("No Fact Provided by Database")
         # TODO: (Task P4.5) Pass the category to the create_fact function
 
         return render_template("create.html", random_fact=factt) # TODO: (Task P1.2) Pass the HTML template, fact and category (Task P4.5) parameters
