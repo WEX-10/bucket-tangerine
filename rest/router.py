d757ad34d04dd9c0e2fb5dc2ad6cee2e5de98e57
WEX 10 <work-experience10@outlook.com>
2026-07-15 14:06:02 +0100

    P1.3 updated router.py

--- rest/router.py
+++ rest/router.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from .home import home_route
 from rest.get_fact import get_route
+from create_fact import create_route 
 # TODO: (Task P1.3) Import the create_route function from the create_fact module
 # TODO: (Task P3.3) Import the vote_fact function from the vote_fact module
 # TODO: (Task P5.5) Import the get_facts_by_category_route function from the get_facts_by_category module
@@ -14,6 +15,7 @@ def create_app():
 
     app.add_url_rule("/", view_func=home_route, methods=["GET"])
     app.add_url_rule("/generate", view_func=get_route, methods=["GET"])
+    app.add_url_rule("/create", view_func=create_route, methods=["GET", "POST"])
     # TODO: (Task P0.3) Add a URL rule for the generate route
     # TODO: (Task P1.3) Add a URL rule for the create route
     # TODO: (Task P3.3) Add a URL rule for the vote route
