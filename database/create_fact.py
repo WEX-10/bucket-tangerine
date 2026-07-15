afb44e502d6275b9c3c0bfe5f1d541ab920ace05
WEX 10 <work-experience10@outlook.com>
2026-07-15 15:02:30 +0100

    fixed faulty sql execution in create_fact.py

--- database/create_fact.py
+++ database/create_fact.py
@@ -10,8 +10,8 @@ def create_fact(fact_text) -> Fact:
     with provider.cursor() as cur:
         # TODO: (Task P3.1) Add likes and dislikes counts to SQL query
         # TODO: (Task P4.3) Add category to SQL query
-        cur.execute("INSERT INTO facts (fact) VALUES(%s,%s) RETURNING (id, fact)",
-                     (fact_text)) # SQL query goes here
+        cur.execute("INSERT INTO facts (fact) VALUES(?) RETURNING id, fact;",
+                     (fact_text,)) # SQL query goes here
         fact_data = fetch_fact_data(cur)
         provider.commit()
         if not fact_data:
