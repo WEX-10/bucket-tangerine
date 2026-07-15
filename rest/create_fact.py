79ab91c903df546c7d887b29ac4a0a64619fe1d6
WEX 10 <work-experience10@outlook.com>
2026-07-15 13:54:09 +0100

    Finished P1.2, rest api create_fact.py

--- rest/create_fact.py
+++ rest/create_fact.py
@@ -6,17 +6,21 @@ from database import create_fact
 def create_route():
     if request.method == "GET":
         # TODO: (Task P1.2) Render the create.html template
+        render_template("create.html")
         pass
 
     if request.method == "POST":
         # TODO: (Task P1.2) Get the fact_text from the form
-        pass
+        factt = request.form.get("fact_text")
 
         # TODO: (Task P4.5) Get the category from the form
 
         # TODO: (Task P1.2) Check that fact text is provided, if not return an error
-        
+        if factt == None:
+            raise ValueError("No Fact Provided")
+
         # TODO: (Task P1.2) Call the create_fact function from the database folder
+        #fact = create_fact(factt)
         # TODO: (Task P4.5) Pass the category to the create_fact function
 
-        return render_template() # TODO: (Task P1.2) Pass the HTML template, fact and category (Task P4.5) parameters
+        return render_template("create.html", random_fact=factt) # TODO: (Task P1.2) Pass the HTML template, fact and category (Task P4.5) parameters
